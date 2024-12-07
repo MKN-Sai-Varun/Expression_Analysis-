21990f70b3e107104458de1dfd1846a41ba41431
Varun <mknsvarun@gmail.com>
2024-12-07 11:07:58 +0530

    Major changes: Added routes, removed deprecations and cleaned login server while adding comments for the same

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,16 +1,17 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+import mongoose from 'mongoose'; // Importing mongoose
+import bcrypt from 'bcryptjs';   // Importing bcrypt for encrypting the entrered password
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    role: { type: String, required: true, enum: ['admin', 'kid'] },  // Creating a user schema consisting of Usename,Pass,role
 });
 
-userSchema.pre('save', async function (next) {
+UserSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
-    this.password = await bcrypt.hash(this.password, 10);
+    this.password = await bcrypt.hash(this.password, 10); // Encrypts the password and saves it
     next();
 });
 
-const User = mongoose.model('User', userSchema);
-module.exports=User;
+const User = mongoose.model('User', UserSchema); // Creating the schema
+export default User; // Exporting
