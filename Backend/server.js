b8ca467bc487d60ce0d670cca920780fbe1da25d
Varun <mknsvarun@gmail.com>
2024-12-09 22:01:58 +0530

    Added routes for triggering model and updated server accordingly

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,7 @@ import { processImages } from './Model.mjs';
 import { MongoClient } from 'mongodb';
 import Counter from './models/Counter.js'; // Import Counter model
 import updateCounter from './routes/update_counter.js';
+import triggerModel from './routes/triggerModel.js';
 import { getSystemErrorMap } from 'util';
 
 dotenv.config();
@@ -393,32 +394,7 @@ async function saveSessionData(pathsArray, sessionCounter, pathType) {
   console.log(`${pathType} successfully saved to MongoDB.`);
 }
 
-// Route to trigger model processing 
-app1.post('/trigger-model', async (req, res) => {
-  try {
-      // const currentSession = await getCurrentCounterValue(); // Retrieve current session value
-      const {Session_Id}=req.body;
-      if (!Session_Id) {
-          return res.status(400).json({ error: 'Did not receive the session_Id' });
-      }
-
-      console.log(`Triggering model for Session ${Session_Id}`);
-      let result=await processImages(Session_Id); // Pass session value to processImages
-      if(result.message==="Data inserted into Mongo Successfully"){
-        console.log("Analysed data inserted into MongoDB");
-        res.status(200).json({message:"Inserted Analysis into MongoDB"});
-      }
-      else if(result.message==="Data insertion failed"){
-        console.log("Failed in inserting data");
-        res.status(400).json({message:"Failed inserting Analysis into MongoDB"});
-
-      }
-      // res.status(200).json({ message: `Model processing triggered for Session ${Session_Id}` });
-  } catch (error) {
-      console.error("Error triggering model:", error);
-      res.status(500).json({ error: 'Failed to trigger model' });
-  }
-});
+app1.use('/',triggerModel);
 
 // Combine `app1` and `app2` under the same server
 const mainApp = express();
